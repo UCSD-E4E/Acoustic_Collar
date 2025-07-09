046b64fd3dfae3bd0716392c2c7640dafacb17cd
Milo Akerman <40443620+MiloAkerman@users.noreply.github.com>
2025-07-09 11:48:15 -0700

    Move _write to user code

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -81,13 +81,12 @@ static void MX_FMC_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-/* USER CODE END 0 */
-
-
 int _write(int file, char *ptr, int len) {
     HAL_UART_Transmit(&huart1, (uint8_t *)ptr, len, HAL_MAX_DELAY);
     return len;
 }
+
+/* USER CODE END 0 */
 
 
 /**
