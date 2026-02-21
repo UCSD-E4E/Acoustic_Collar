b3fce4dccd30af3dffe21b2447d55afee8cf649d
mohakvni <mohakvaswani@gmail.com>
2026-02-20 16:47:46 -0800

    Cleaned up a bit

--- CM7/Core/Src/audiohandler.c
+++ CM7/Core/Src/audiohandler.c
@@ -57,61 +57,61 @@ void MicrophoneStartProcess()
 	AudioInInit.Volume = VolumeLevel;
 
 	/* Initialize Audio Recorder with 2 channels to be used */
-	printf("DEBUG: About to call BSP_AUDIO_IN_Init(1, ...)\n");
+//	printf("DEBUG: About to call BSP_AUDIO_IN_Init(1, ...)\n");
 	int32_t ret = BSP_AUDIO_IN_Init(1, &AudioInInit);
-	printf("DEBUG: BSP_AUDIO_IN_Init returned: %ld\n", ret);
+//	printf("DEBUG: BSP_AUDIO_IN_Init returned: %ld\n", ret);
 	
 	ret = BSP_AUDIO_IN_GetState(1, &InState);
-	printf("DEBUG: BSP_AUDIO_IN_GetState returned: %ld\n", ret);
+//	printf("DEBUG: BSP_AUDIO_IN_GetState returned: %ld\n", ret);
 	
-	printf("DEBUG: About to call BSP_AUDIO_OUT_Init(0, ...)\n");
+//	printf("DEBUG: About to call BSP_AUDIO_OUT_Init(0, ...)\n");
 	ret = BSP_AUDIO_OUT_Init(0, &AudioOutInit);
-	printf("DEBUG: BSP_AUDIO_OUT_Init returned: %ld\n", ret);
+//	printf("DEBUG: BSP_AUDIO_OUT_Init returned: %ld\n", ret);
 
-	printf("DEBUG: About to call BSP_AUDIO_OUT_SetDevice\n");
+//	printf("DEBUG: About to call BSP_AUDIO_OUT_SetDevice\n");
 	ret = BSP_AUDIO_OUT_SetDevice(0, AUDIO_OUT_DEVICE_HEADPHONE);
-	printf("DEBUG: BSP_AUDIO_OUT_SetDevice returned: %ld\n", ret);
+//	printf("DEBUG: BSP_AUDIO_OUT_SetDevice returned: %ld\n", ret);
 	
-	printf("DEBUG: About to call BSP_AUDIO_IN_RecordPDM - THIS IS WHERE IT HANGS\n");
+//	printf("DEBUG: About to call BSP_AUDIO_IN_RecordPDM - THIS IS WHERE IT HANGS\n");
 	// Ensure SAI4 is enabled
 	__HAL_SAI_ENABLE(&haudio_in_sai);
-	printf("DEBUG: SAI4 enabled\n");
+//	printf("DEBUG: SAI4 enabled\n");
 	
 	// Diagnostic info
-	HAL_SAI_StateTypeDef sai_state = haudio_in_sai.State;
-	printf("DEBUG: SAI State before RecordPDM: %d\n", sai_state);
-	printf("DEBUG: haudio_in_sai Instance: 0x%lx\n", (uint32_t)haudio_in_sai.Instance);
-	printf("DEBUG: haudio_in_sai.hdmarx: 0x%lx\n", (uint32_t)haudio_in_sai.hdmarx);
-	
-	if(haudio_in_sai.hdmarx != NULL) {
-		printf("DEBUG: DMA Instance: 0x%lx\n", (uint32_t)haudio_in_sai.hdmarx->Instance);
-		printf("DEBUG: DMA State: %d\n", haudio_in_sai.hdmarx->State);
-	} else {
-		printf("DEBUG: ERROR - hdmarx is NULL!\n");
-	}
+//	HAL_SAI_StateTypeDef sai_state = haudio_in_sai.State;
+//	printf("DEBUG: SAI State before RecordPDM: %d\n", sai_state);
+//	printf("DEBUG: haudio_in_sai Instance: 0x%lx\n", (uint32_t)haudio_in_sai.Instance);
+//	printf("DEBUG: haudio_in_sai.hdmarx: 0x%lx\n", (uint32_t)haudio_in_sai.hdmarx);
+//
+//	if(haudio_in_sai.hdmarx != NULL) {
+//		printf("DEBUG: DMA Instance: 0x%lx\n", (uint32_t)haudio_in_sai.hdmarx->Instance);
+//		printf("DEBUG: DMA State: %d\n", haudio_in_sai.hdmarx->State);
+//	} else {
+//		printf("DEBUG: ERROR - hdmarx is NULL!\n");
+//	}
 	
 	// Check Audio_In_Ctx
-	printf("DEBUG: Audio_In_Ctx[1].BitsPerSample: %ld\n", Audio_In_Ctx[1].BitsPerSample);
-	uint32_t dma_size = (AUDIO_IN_PDM_BUFFER_SIZE / (Audio_In_Ctx[1].BitsPerSample/8U));
-	printf("DEBUG: DMA transfer size (half-words): %ld\n", dma_size);
+//	printf("DEBUG: Audio_In_Ctx[1].BitsPerSample: %ld\n", Audio_In_Ctx[1].BitsPerSample);
+//	uint32_t dma_size = (AUDIO_IN_PDM_BUFFER_SIZE / (Audio_In_Ctx[1].BitsPerSample/8U));
+//	printf("DEBUG: DMA transfer size (half-words): %ld\n", dma_size);
 	
 	// Check PDM configuration
-	SAI_Block_TypeDef* sai_block = (SAI_Block_TypeDef*)haudio_in_sai.Instance;
-	printf("DEBUG: SAI CR1: 0x%lx\n", sai_block->CR1);
-	printf("DEBUG: SAI FRCR: 0x%lx\n", sai_block->FRCR);
-	printf("DEBUG: SAI SLOTR: 0x%lx\n", sai_block->SLOTR);
-	
-	printf("DEBUG: Calling HAL_SAI_Receive_DMA...\n");
+//	SAI_Block_TypeDef* sai_block = (SAI_Block_TypeDef*)haudio_in_sai.Instance;
+//	printf("DEBUG: SAI CR1: 0x%lx\n", sai_block->CR1);
+//	printf("DEBUG: SAI FRCR: 0x%lx\n", sai_block->FRCR);
+//	printf("DEBUG: SAI SLOTR: 0x%lx\n", sai_block->SLOTR);
+//
+//	printf("DEBUG: Calling HAL_SAI_Receive_DMA...\n");
 	
 	// Try to start DMA with interrupt-based approach instead of blocking
 	// Use HAL_SAI_Receive_DMA directly with a timeout
-	uint32_t tickstart = HAL_GetTick();
-	uint32_t timeout_ms = 1000; // 1 second timeout
+//	uint32_t tickstart = HAL_GetTick();
+//	uint32_t timeout_ms = 1000; // 1 second timeout
 	
 	// Call the DMA receive with explicit timeout check
 	HAL_StatusTypeDef hal_ret = HAL_SAI_Receive_DMA(&haudio_in_sai, (uint8_t*)&recordPDMBuf, AUDIO_IN_PDM_BUFFER_SIZE / 2);
 	printf("DEBUG: HAL_SAI_Receive_DMA returned HAL status: %d\n", hal_ret);
-	
+
 	if(hal_ret != HAL_OK) {
 		printf("DEBUG: ERROR! HAL_SAI_Receive_DMA failed with status: %d\n", hal_ret);
 		printf("DEBUG: SAI State after failure: %d\n", haudio_in_sai.State);
@@ -124,10 +124,12 @@ void MicrophoneStartProcess()
 		ret = BSP_ERROR_NONE;
 	}
 
+//	BSP_AUDIO_IN_RecordPDM(1, (uint8_t*)&recordPDMBuf, AUDIO_IN_PDM_BUFFER_SIZE/2);
+
 	// TODO: remove audio playback - just for testing audio recording
-	printf("DEBUG: About to call BSP_AUDIO_OUT_Play\n");
+//	printf("DEBUG: About to call BSP_AUDIO_OUT_Play\n");
 	ret = BSP_AUDIO_OUT_Play(0, (uint8_t*)&RecPlayback, RECORD_BUFFER_SIZE);
-	printf("DEBUG: BSP_AUDIO_OUT_Play returned: %ld\n", ret);
+//	printf("DEBUG: BSP_AUDIO_OUT_Play returned: %ld\n", ret);
 }
 
 void DMA2_Stream1_IRQHandler(void)
@@ -153,7 +155,7 @@ void  BSP_AUDIO_IN_TransferComplete_CallBack(uint32_t Instance)
     SCB_CleanDCache_by_Addr((uint32_t*)&RecPlayback[playbackPtr], AUDIO_IN_PDM_BUFFER_SIZE/4);
 
     // TODO: MAKE A FLAG TO AVOID CALLING IN TRANSFER CALLBACK
-    printf("processing 2nd half of buffer");
+//    printf("processing 2nd half of buffer");
     AI_PROCESS = 1; //signal AI processing
     //MX_X_CUBE_AI_Process(&RecPlayback[playbackPtr]);
 
@@ -163,6 +165,7 @@ void  BSP_AUDIO_IN_TransferComplete_CallBack(uint32_t Instance)
   }
   else
   {
+	printf("HERE\n");
     AudioBufferOffset = BUFFER_OFFSET_FULL;
   }
 
@@ -186,7 +189,7 @@ void BSP_AUDIO_IN_HalfTransfer_CallBack(uint32_t Instance)
     SCB_CleanDCache_by_Addr((uint32_t*)&RecPlayback[playbackPtr], AUDIO_IN_PDM_BUFFER_SIZE/4);
 
     // TODO: MAKE A FLAG TO AVOID CALLING IN TRANSFER CALLBACK
-    printf("processing 1st half of buffer");
+//    printf("processing 1st half of buffer");
     AI_PROCESS = 1; //signal AI processing
     //MX_X_CUBE_AI_Process(&RecPlayback[playbackPtr]);
 
